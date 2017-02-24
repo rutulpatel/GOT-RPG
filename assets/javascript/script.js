ea372a2d470027369ec52c8335f5eeb603bf2c0a
Rutul Patel <rutul-patel@live.com>
2017-02-23 16:44:20 -0800

    Add character containers for handling through js

---
+++ assets/javascript/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    //variables
+    var playerRandomAdder = 0;
+
+
+    var player = {
+        getHP: function() {
+            return
+        }
+    };
+
+    var opponent = {
+
+    };
+
+});
